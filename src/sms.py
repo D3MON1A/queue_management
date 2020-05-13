87fb13a6892da5357cd375a9e10666a92343292d
leisyarcia <leisyarcia@gmail.com>
2020-05-13 14:33:12 +0000

    Final

--- src/sms.py
+++ src/sms.py
@@ -3,8 +3,7 @@ from twilio.rest import Client
 import os
 
 def send(body='Some body', to=''):
-    # Your Account Sid and Auth Token from twilio.com/console
-    # DANGER! This is insecure. See http://twil.io/secure
+    
     account_sid = os.getenv("sid")
     auth_token = os.getenv("token")
     client = Client(account_sid, auth_token)
